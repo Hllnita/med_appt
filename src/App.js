a7ea8b12157db457c70351b34e399bdd2cc10bea
hll <hninlwinlwin2017@gmail.com>
2025-04-30 04:25:34 -0400

    add navbar and landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// Import necessary modules from React library
+// import React, { useEffect } from 'react';
 
+// Import components for routing from react-router-dom library
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Import custom Navbar component
+import Navbar from './Components/Navbar/Navbar';
+import LandingPage from './Components/LandingPage/LandingPage';
+
+
+// Function component for the main App
 function App() {
+
+  // Render the main App component
   return (
     <div className="App">
+        {/* Set up BrowserRouter for routing */}
         <BrowserRouter>
-          <Notification>
-              <Routes>
-                <Route path="/login" element={<Login />}/>
-                <Route path="/signup" element={<SignUp />}/>
-                <Route path="/instant-consultation" element={<InstantConsultation />} />
-                 <Route path="<component_route>" element={<component_name/>}/> //Replace the component_route with the component path and component_name with the component name as imported in the App.js file. 
-                />
-              </Routes>
-          </Notification>
+          {/* Display the Navbar component */}
+          <Navbar/>
+
+          {/* Set up the Routes for different pages */}
+          <Routes>
+            {/* Define individual Route components for different pages */}
+            <Route path="/" element={<LandingPage/>}/>
+          </Routes>
         </BrowserRouter>
     </div>
   );
 }
 
+// Export the App component as the default export
 export default App;
