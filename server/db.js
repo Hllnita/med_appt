c76e952d49577b7df2d6f2642ff37543179cf4f4
hll <hninlwinlwin2017@gmail.com>
2025-04-30 03:29:25 -0400

    add server initiliazed dbjs'

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:C4gzU0bG7CPhrJ2wVsJK6wwb@172.21.79.16:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
