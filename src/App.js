2f7b52ec9eb9457b6c62afaa1fb791283a75e1fc
hll <hninlwinlwin2017@gmail.com>
2025-04-30 03:48:33 -0400

    add notification

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Notification>
+              <Routes>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/signup" element={<SignUp />}/>
+                <Route path="/instant-consultation" element={<InstantConsultation />} />
+                 <Route path="<component_route>" element={<component_name/>}/> //Replace the component_route with the component path and component_name with the component name as imported in the App.js file. 
+                />
+              </Routes>
+          </Notification>
+        </BrowserRouter>
     </div>
   );
 }
